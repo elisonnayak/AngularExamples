93c5c3f193718e661867315bb9ebba7712697790
Elison Nayak <nayakelison@gmail.com>
2024-11-21 23:59:22 -0500

    Added kitchen sink index page

--- angularConceptsAtoZ/src/app/app.module.ts
+++ angularConceptsAtoZ/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatListModule } from '@angular/material/list'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KitchenSinkComponent } from './kitchen-sink/kitchen-sink.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KitchenSinkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angularConceptsAtoZ/src/app/kitchen-sink/kitchen-sink.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen-sink',
+  templateUrl: './kitchen-sink.component.html',
+  styleUrls: ['./kitchen-sink.component.scss']
+})
+export class KitchenSinkComponent {
+
+}
