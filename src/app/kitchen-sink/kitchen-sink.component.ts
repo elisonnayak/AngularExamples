b8a15a0141d9c3dbab9bc2853bc6b5ceb0ca81ae
Elison Nayak <nayakelison@gmail.com>
2024-12-02 23:05:06 -0500

    Examples added

--- src/app/kitchen-sink/kitchen-sink.component.ts
+++ src/app/kitchen-sink/kitchen-sink.component.ts
@@ -3,7 +3,8 @@ interface Topic {
   topicId: number,
   title: string,
   description: string,
-  examples: Array<{exampleName: string,exampleId: string,fileName: string}>,
+  shortDescription: string,
+  examples: Array<{exampleName: string, projectId: string, fileName: string}>,
   type: string
 }
 @Component({
@@ -24,49 +25,57 @@ export class KitchenSinkComponent{
 
   intialListOfTopics: Array<Topic> = [
     {
-      "topicId": 100,
-      "title": "Dynamic import",
-      "description": "Dynamic import helps to load some components dynamically. This is a way to lazy load a component.",
+      "topicId": 101,
+      "title": "Two pointers pattern",
+      "shortDescription": "Two pointers pattern provides value at first and last index. The index positions will change based on condition.",
+      "description": `Provided a sorted array in ascending order. Initialize two indices, one pointing the first element in the array and another to the last element. 
+      Find sum of elements for both pointers. Move the left pointer to right if the sum is less than target sum. 
+      Simillarly move the right pointer to left if the sum is greater than the target sum.`,
       "examples": [
         {
-          'exampleName': 'Dynamic Import',
-          'exampleId': '',
-          'fileName': ''
+          'exampleName': 'Find the indices of two elements which sum equal to a target sum',
+          'projectId': 'vitejs-vite-vuzidy',
+          'fileName': 'src/twoPointersPattern.js'
         }, {
           'exampleName': 'Example 2',
-          'exampleId': '',
+          'projectId': '',
           'fileName': ''
         }, {
           'exampleName': 'Example 3',
-          'exampleId': '',
+          'projectId': '',
           'fileName': ''
         }
       ],
-      "type":"angular"
+      "type":"dsa"
     },
     {
-      "topicId": 101,
-      "title": "Prefix Sum Pattern",
+      "topicId": 100,
+      "title": "Prefix and Suffix Sum Pattern",
+      "shortDescription": "Prefix and suffix sum provides the arrays having sum at each index in forward anf backward direction",
       "description": `This represent a pattern to derive an array of elements with 
       summation data at each index equals to sum of current value and previous value.`,
       "examples": [
         {
           'exampleName': 'Prefix Sum Pattern',
-          'exampleId': 'vitejs-vite-vuzidy',
-          'fileName': 'src/counter.js'
+          'projectId': 'vitejs-vite-vuzidy',
+          'fileName': 'src/prefixSuffixSum.js'
+        }, {
+          'exampleName': 'Suffix Sum Pattern',
+          'projectId': 'vitejs-vite-vuzidy',
+          'fileName': 'src/prefixSuffixSum.js'
         }, {
           'exampleName': 'Equilibrium Index',
-          'exampleId': 'vitejs-vite-vuzidy',
+          'projectId': 'vitejs-vite-vuzidy',
           'fileName': 'src/equilibriumIndex.js'
         }, {
           'exampleName': 'Example 3',
-          'exampleId': '',
+          'projectId': '',
           'fileName': ''
         }
       ],
       "type": "dsa"
     }
-  ];
+  ].sort((a,b) => a.topicId - b.topicId);
 
   listOfTopics: Array<Topic> = [...this.intialListOfTopics];
 
